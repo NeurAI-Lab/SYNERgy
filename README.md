fd7d1a49fe67d5c6f05366203f7718b033fabd28
Fahad <fahad.sarfraz@navinfo.eu>
2022-06-07 15:22:21 +0200

    Init repo

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# SYNERgy
-The official PyTorch code for CoLLAs 2022 paper "SYNERgy between SYNaptic consolidation and Experience Replay for general continual learning"
+# SYNERgy 
+Official Repository for CoLLAs 2022 paper "SYNERgy between SYNaptic consolidation and Experience Replay for general continual learning"
+
+We extended the [CLS-ER](https://github.com/NeurAI-Lab/CLS-ER) repo with our method (SYNERgy)
+
+## Setup
+
++ Use `python main.py` to run experiments.
++ Use argument --load_best_args to use the best hyperparameters for each of the evaluation setting from the paper.
++ To reproduce the results for SYNERgy in the paper run the following
+    `python main.py --dataset <dataset> --model synergy --buffer_size <buffer_size> --load_best_args`
+
+ Examples:
+
+    python main.py --dataset rot-mnist --model synergy --buffer_size 500 --load_best_args
+    
+    python main.py --dataset seq-cifar10 --model synergy --buffer_size 500 --load_best_args
+    
+    python main.py --dataset seq-tinyimg --model synergy --buffer_size 500 --load_best_args
+
+    python main.py --dataset gcil-cifar100 --weight_dist unif --model synergy --load_best_args
+    
+    python main.py --dataset gcil-cifar100 --weight_dist longtail --model synergy --load_best_args
+    
+
+## Requirements
+
+- torch==1.7.0
+
+- torchvision==0.9.0 
+
+- quadprog==0.1.7
---
+++ models/ewc_on.py
@@ -0,0 +1,116 @@
+# Copyright 2020-present, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Davide Abati, Simone Calderara.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from utils.args import *
+from models.utils.continual_model import ContinualModel
+
+
+def get_parser() -> ArgumentParser:
+    parser = ArgumentParser(description='Continual learning via online EWC.')
+    add_management_args(parser)
+    add_experiment_args(parser)
+    parser.add_argument('--e_lambda', type=float, required=True,
+                        help='lambda weight for EWC')
+    parser.add_argument('--gamma', type=float, required=True,
+                        help='gamma parameter for EWC online')
+    parser.add_argument('--adjust_fisher', action='store_true', default=False,  help='gamma parameter for EWC online')
+    return parser
+
+
+class EwcOn(ContinualModel):
+    NAME = 'ewc_on'
+    COMPATIBILITY = ['class-il', 'domain-il', 'task-il']
+
+    def __init__(self, backbone, loss, args, transform):
+        super(EwcOn, self).__init__(backbone, loss, args, transform)
+
+        self.logsoft = nn.LogSoftmax(dim=1)
+        self.checkpoint = None
+        self.fish = None
+        self.task_id = 0
+
+    def penalty(self):
+        if self.checkpoint is None:
+            return torch.tensor(0.0).to(self.device)
+        else:
+            penalty = (self.fish * ((self.net.get_params() - self.checkpoint) ** 2)).sum()
+            return penalty
+
+    def end_task(self, dataset):
+        self.task_id += 1
+        fish = torch.zeros_like(self.net.get_params())
+
+        for j, data in enumerate(dataset.train_loader):
+            inputs, labels, _ = data
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+            for ex, lab in zip(inputs, labels):
+                self.opt.zero_grad()
+                output = self.net(ex.unsqueeze(0))
+                loss = - F.nll_loss(self.logsoft(output), lab.unsqueeze(0),
+                                    reduction='none')
+                exp_cond_prob = torch.mean(torch.exp(loss.detach().clone()))
+                loss = torch.mean(loss)
+                loss.backward()
+                fish += exp_cond_prob * self.net.get_grads() ** 2
+
+        fish /= (len(dataset.train_loader) * self.args.batch_size)
+
+        if self.fish is None:
+            self.fish = fish
+        else:
+            self.fish *= self.args.gamma
+            self.fish += fish
+
+        if self.args.adjust_fisher:
+            print('Adjusting Fisher')
+            self.fish = self.adjust_fisher()
+
+        self.checkpoint = self.net.get_params().data.clone()
+
+    def observe(self, inputs, labels, not_aug_inputs):
+
+        self.opt.zero_grad()
+        outputs = self.net(inputs)
+        penalty = self.penalty()
+        loss = self.loss(outputs, labels) + self.args.e_lambda * penalty
+        assert not torch.isnan(loss)
+        loss.backward()
+        self.opt.step()
+
+        return loss.item()
+
+    def count_params(self, param_shape):
+        count = 1
+        for dim in param_shape:
+            count *= dim
+        return count
+
+    def adjust_fisher(self):
+        adjusted_fisher = self.fish.clone()
+        start_idx = 0
+        total_params = 0
+        for name, param in self.net.named_parameters():
+            param_count = self.count_params(list(param.shape))
+            if ('conv' in name or 'shortcut' in name) and len(param.shape) > 1:
+                # print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+                # print(name, list(param.shape), start_idx, start_idx + param_count)
+                # print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+                num_filters = param.shape[0]
+                params_per_filter = param.shape[1] * param.shape[2] * param.shape[3]
+
+                for filter_idx in range(num_filters):
+                    filter_start_idx = start_idx + (filter_idx * params_per_filter)
+                    filter_end_idx = start_idx + ((filter_idx + 1) * params_per_filter)
+                    filter_params = adjusted_fisher[filter_start_idx: filter_end_idx]
+                    imp_val = torch.mean(filter_params)
+                    adjusted_fisher[filter_start_idx: filter_end_idx] = imp_val
+
+            start_idx += param_count
+            total_params += param_count
+
+        return adjusted_fisher
